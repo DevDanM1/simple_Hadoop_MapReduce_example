387833a473083485ec63234b92ea89917899f6e6
devdan1234567@gmail.com <devdan1234567@gmail.com>
2021-08-25 04:29:56 +0000

    updated mapper to remove punctuation and stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 import sys
 
+stops = set(['the', 'and', 'is', 'or', 'not', 'for', 'i'])
+
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+
+    # remove punctuation
+    line = line.replace('!','')
+    line = line.replace('?','')
+    line = line.replace(':','')
+    line = line.replace(';','')
+    line = line.replace('.','')
+    line = line.replace(',','')
+
+    # make lower case
+    line = line.lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stops:
+            print '%s\t%s' % (word, "1")
